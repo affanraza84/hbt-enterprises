3f531a30d1b1cbd86769ac85a3a5f5016852aeec
Affan Raza <affanraza8081@gmail.com>
2026-02-14 02:08:37 +0530

    feat: update image for Dia Sun 10kg blue washing machine

--- src/data/images/washingMachines.ts
+++ src/data/images/washingMachines.ts
@@ -6,7 +6,7 @@ export const WASHING_MACHINE_IMAGES: Record<string, string[]> = {
     'diasun-wm-11-full': ['/images/washing-machines/wm-11kg-black.jpeg'],
     'diasun-wm-85-full': ['/images/washing-machines/wm-8.5kg-black-orange.jpeg'],
     'diasun-wm-75-opaque': ['/images/washing-machines/wm-7.5kg-grey.jpeg'],
-    'diasun-wm-10-full': ['/images/washing-machines/wm-10kg-blue.jpeg'],
+    'diasun-wm-10-full': ['/images/washing-machines/wm-10kg-blue-new.jpeg'],
     'diasun-wm-105-grey': ['/images/washing-machines/wm-10.5kg-grey-glass.jpeg'],
     'diasun-wm-95-gold': ['/images/washing-machines/wm-9.5kg-gold-glass.jpeg'],
     'diasun-wm-75-maroon': ['/images/washing-machines/wm-7.5kg-maroon-opaque.jpeg'],
